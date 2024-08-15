77b690fc8e79a3ed3f310a71560bb1ef1fdda5a9
Olof333 <52601451+Olof333@users.noreply.github.com>
2024-08-15 23:43:50 +0300

    models and db

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -1,8 +1,25 @@
 from pydantic import BaseModel
 
 class ItemCreate(BaseModel):
-    name = str
-    description = str
-    category = str
-    quantity = str
-    price = float
+    name: str
+    description: str
+    category: str
+    quantity: str
+    price: float
+
+
+class ItemUpdate(BaseModel):
+    name: str
+    description: str
+    category: str
+    quantity: str
+    price: float
+
+
+class ItemReturn(BaseModel):
+    id: int
+    name: str
+    description: str
+    category: str
+    quantity: str
+    price: float
