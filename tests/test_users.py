c765b877a7844c2eb2470dad393aff251b59c4f1
Olof333 <fleksovich@gmail.com>
2024-09-04 16:07:29 +0300

    tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,20 +1,18 @@
-from unittest.mock import MagicMock
-from fastapi.testclient import TestClient
 from app import app
+from fastapi.testclient import TestClient
 
 
 client = TestClient(app)
 
 
-def test_create_user(mocker):
-    mock_db_session = MagicMock()
-    mocker.patch("app.db.get_db", return_value=mock_db_session)
-
-    new_user = {"email": "test@gmail.com", "hashed_pass": "123456"}
-    response = client.post("/users/", json=new_user)
+def test_create_user(mock_db_session):
+    data = {"email": "test@gmail.com", "hashed_pass": "string"}
+    response = client.post("/users/", data=data)
 
     assert response.status_code == 200
-    assert response.json()["email"] == "testuser@example.com"
+    data = response.json()
 
-    mock_db_session.add.assert_called_once()
-    mock_db_session.commit.assert_called_once()
+    assert data["email"] == "test@gmail.com"
+
+    mock_db_session.add.assert_called()
+    mock_db_session.commit.assert_called()
